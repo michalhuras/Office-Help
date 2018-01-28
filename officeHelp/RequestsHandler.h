5dd2e514dcf1452ff685cefae573c32e05ca6616
Michał <michal.huras94@gmail.com>
2018-01-28 19:57:15 +0100

    Inicjalizacja requestHendlera

--- officeHelp/RequestsHandler.h
+++ officeHelp/RequestsHandler.h
@@ -6,13 +6,21 @@
 
 class RequestsHandler
 {
+
+private:
 	QVector<QString> listOfFiles;
+	QString catalogPath;
+	Qstring wordToSearch;
+	searchMode choosenSearchMode;
 public:
+	enum searchMode {
+		error = -1,
+		inThisCatalog = 0,
+		inThisCatalogRecursevely = 1,
+		inThisFile = 2};
+
 	RequestsHandler();
 	void createFileList();
-
-
-
 };
 
 #endif // REUESTSHANDLER_H
